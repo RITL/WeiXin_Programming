b5e4b3c14cb8cf7a6e6fa0fc1221e3bbe6ae58cd
774655048@qq.com <774655048@qq.com>
2017-11-08 10:47:01 +0800

    add DetailModule

---
+++ ReaderMovie03_Detailed/pages/posts/posts.js
@@ -0,0 +1,40 @@
+var postsData = require('../../data/posts-data.js');
+// var postsData = require('/data/posts-data.js');
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        //小程序总是会读取data对象来做数据绑定，这个动作为动作A
+        //A的执行，在onload事件执行之后发生
+    },
+
+
+    onPostTap: function(event) {
+
+        var postId = event.currentTarget.dataset.postId;
+        // console.log(postId);
+        wx.navigateTo({
+            url: 'post-detail/post-detail',
+            success: function(res) {},
+            fail: function(res) {},
+            complete: function(res) {},
+        })
+    },
+
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        // console.log("onload");
+        //放置服务器请求
+        // var posts_contents_data = { "posts_key": posts_content }
+        this.setData(
+            {
+                "posts_key": postsData.postList
+            });
+    },
+})
---
+++ ReaderMovie03_Detailed/pages/welcome/welcome.js
@@ -0,0 +1,81 @@
+Page({
+    // "#405f80"
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+
+  onTextTap: function(){
+    //   console.log("on text tap");
+  },
+
+  onTap: function (){
+    //   console.log("on tap");
+        // wx.navigateTo({//进行导航跳转，进入导航的子视图
+        //     url: "../posts/posts",
+        // });
+
+        wx.redirectTo({//没有返回按钮，同级界面进行跳转
+            url: "../posts/posts",
+        })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+      console.log("welcome onhide");
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    console.log("welcome unload");
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
